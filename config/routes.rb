70b16346a53d6b3d3a06058c952cd10a2258358d
rishi90kesh <rishikesh.singh@ia.ooo>
2020-01-03 00:32:01 +0530

    kasbdv

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+   collection do
+     post "/login", to: "users#login"
+   end
+ end
+
+ resources :videos do
+   collection do
+     get "/view_count_update/:id", to: "videos#view_count_update"
+     get "/search", to: "videos#video_search"
+   end
+ end
 end
