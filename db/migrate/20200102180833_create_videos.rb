2fa8d456f439585e87d85334da4994cd08c9f390
rishi90kesh <rishikesh.singh@ia.ooo>
2020-01-02 23:52:14 +0530

    sdjkbv

---
+++ db/migrate/20200102180833_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :videos do |t|
+      t.string :title, null: false
+     t.string :src, null: false
+     t.string :content_type, null: false
+     t.string :poster_src, null:false
+     t.string :views, null:false
+     
+      t.timestamps
+    end
+  end
+end
